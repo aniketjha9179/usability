1defe7481c83756bed561216ec1727315718c3c3
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2023-03-22 14:30:39 +0530

    Update indes.js

--- indes.js
+++ indes.js
@@ -64,3 +64,20 @@ from(['examples-card','examples-area flex'],{
     x:31,
     opacity:0,
 })
+
+const accrodians = document.querySelectorAll(".accrodian");
+
+accrodians.forEach(accrodian => {
+  const icon = accrodian.querySelector(".icon");
+  const answer = accrodian.querySelector(".answer");
+
+  accrodian.addEventListener("click", () => {
+    if (icon.classList.contains("active")) {
+      icon.classList.remove("active");
+      answer.style.maxHeight = null;
+    } else {
+      icon.classList.add("active");
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+});
